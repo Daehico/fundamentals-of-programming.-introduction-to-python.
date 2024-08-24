11507768283c4d31b364a5dee40815f33c10213c
Daehico <empire_95@mail.ru>
2024-08-24 19:38:07 +0300

    Update code style

--- secondModuleFirstTasks.py
+++ secondModuleFirstTasks.py
@@ -1,21 +1,31 @@
+import math
+
 #Task 1
-smallBottlesCost = 0.10
-bigBottlesCost = 0.25
+small_bottles_cost = 0.10
+big_bottles_cost = 0.25
 
-smallBottlesCount = int(input("Введите количество бутылок объемом 1 литр и меньше : "))
-bigBottlesCount = int(input("Введите количество бутылок объемом больше 1 литра: "))
+small_bottles_count = int(input("Введите количество бутылок объемом 1 литр и меньше : "))
+big_bottles_count = int(input("Введите количество бутылок объемом больше 1 литра: "))
 
-totalSmallBottlesCost = smallBottlesCost * smallBottlesCount
-totalBigBottlesCost = bigBottlesCost * bigBottlesCount
+total_small_bottles_cost = small_bottles_cost * small_bottles_count
+total_big_bottles_cost = big_bottles_cost * big_bottles_count
 
-sum = totalSmallBottlesCost + totalBigBottlesCost
+sum = total_small_bottles_cost + total_big_bottles_cost
 
-formatedSum = "${:.2f}".format(sum)
-print("За сдачу стелотары вы получите" ,formatedSum)
+formated_sum = "${:.2f}".format(sum)
+print("За сдачу стелотары вы получите" ,formated_sum)
 
 #Task 2
 number = int(input("Введите положительное число: "))
 
-sumOfPositiveIntegers = number * (number + 1) / 2
+sum_of_positive_integers = number * (number + 1) / 2
 
-print("Сумма первых ",number," положительных чисел равна ", sumOfPositiveIntegers)
+print("Сумма первых ",number," положительных чисел равна ", sum_of_positive_integers)
+
+#Task 3
+s = int(input("Введите длинну стороны: "))
+n = int(input("Введите количество сторон: "))
+
+regular_polygon_area = (n * s**2) / (4 * math.tan(math.pi/n))
+
+print("Площадь правильного многоугольника: ", regular_polygon_area)
