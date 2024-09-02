6fff63023ce59a1aedc57e96823d621e14b337d0
Daehico <empire_95@mail.ru>
2024-09-02 04:50:11 +0300

    Complete task 3

--- ThirdLessonPracticeTask.py
+++ ThirdLessonPracticeTask.py
@@ -1,3 +1,5 @@
+import random
+
 # Task 1
 BABY_LIMIT = 2
 CHILD_LIMIT = 12
@@ -24,3 +26,34 @@ while line != "":
     line = input("Введите возвраст посетителя (пустая строка для выхода: ")
 
 print("Итоговая цена: ", total_price)
+
+#Task 2
+entered_numbers = []
+
+user_input = int(input("Введите числа для добавления в список: (для выхода введите 0): "))
+
+while user_input != 0:
+    entered_numbers.append(user_input)
+    user_input = int(input("Введите числа для добавления в список: (для выхода введите 0): "))
+
+entered_numbers.sort()
+print(entered_numbers)
+
+#Task 3
+MIN_NUMBER = 1
+MAX_NUMBER = 49
+MAX_LOTTERY_NUMBERS = 6
+
+ticket_numbers = set()
+i = 0
+
+while len(ticket_numbers) < MAX_LOTTERY_NUMBERS:
+    ticket_numbers.add(random.randrange(MIN_NUMBER, MAX_NUMBER + 1))
+    i += 1
+
+ticket_numbers = sorted(ticket_numbers)
+
+print("Номер лотерейного биллета: ", end=" ")
+
+for ticket_number in ticket_numbers:
+    print(ticket_number, end=" ")
